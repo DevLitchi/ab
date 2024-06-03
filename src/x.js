f026739e207f92a40541c79ddf8b43bb15eadacf
DevLitchi <dali.basilio@uabc.edu.mx>
2024-06-03 02:56:43 -0700

    Inicio del Server

--- src/x.js
+++ src/x.js
@@ -1,7 +1,23 @@
-import puppeteer from "puppeteer";
-import fs from "fs";
 import readline from "readline";
-import {scrapeo} from "./NewF.js";
+import { scrapeo } from "./NewF.js";
+import { procesarDatosPeliculas } from "./a.js";
 
+// Pedir rango de años
+async function rangoAños() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
-scrapeo(2015,2015);
+  rl.question('Ingrese el año de inicio: ', async (inicio) => {
+    rl.question('Ingrese el año de fin: ', async (fin) => {
+      rl.close();
+      await scrapeo(inicio, fin); // Esperar que scrapeo termine antes de cerrar la interfaz
+      await procesarDatosPeliculas(); // Llamar a procesarDatosPeliculas después de que scrapeo haya terminado
+    });
+  });
+
+}
+
+// Llamar a la función rangoAños
+rangoAños();
