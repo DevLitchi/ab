9e96bb4611f0ff7d7fbcd5749528aabc1023a8c6
DevLitchi <dali.basilio@uabc.edu.mx>
2024-06-03 17:06:37 -0700

    EL MERO MERO PERRO

--- test2.js
+++ test2.js
@@ -282,7 +282,7 @@ async function procesarDatosPeliculas() {
 // Llamar a la función para procesar los datos de películas
 
 async function main() {
-    // await scrapeo(2015, 2024);
+    await scrapeo(2015, 2024);
     await procesarDatosPeliculas();
 }
 
